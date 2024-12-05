7167c784f912fef6495f0ec33652febcf67f6ff5
Gustavo pontes <gustavo.4dev@gmail.com>
2024-12-05 09:07:30 -0300

    feat: replace Header component with styled Header in UnternehmenTemplate

--- src/templates/unternehmen/index.tsx
+++ src/templates/unternehmen/index.tsx
@@ -3,7 +3,6 @@
 import { type FC } from "react";
 import C from "./constants";
 import * as S from "./styles";
-import { Header } from "@/components/organisms/header";
 import { Accounting } from "@/components/atoms/accounting";
 import { Article } from "@/components/atoms/article";
 import { Advantages } from "@/components/atoms/advantages";
@@ -11,7 +10,7 @@ import { Advantages } from "@/components/atoms/advantages";
 export const UnternehmenTemplate: FC = () => {
   return (
     <>
-      <Header {...C} />
+      <S.Header {...C} />
       <S.Accounting>
         <Accounting {...C.accounting} />
         <Article imgs={C.content.img}>{C.content.text}</Article>
--- src/templates/unternehmen/styles.ts
+++ src/templates/unternehmen/styles.ts
@@ -1,5 +1,16 @@
 import { mediaMaxWidth } from "@/utils/media-query";
 import styled from "styled-components";
+import { Header as _Header } from "@/components/organisms/header";
+import { Title } from "@/components/organisms/header/styles";
+
+export const Header = styled(_Header)`
+  ${Title} {
+    ${mediaMaxWidth("mobile")`
+      font-size: 3.7rem;
+      line-height: 5rem;
+    `}
+  }
+`;
 
 export const Accounting = styled.section`
   margin-top: 10rem;
